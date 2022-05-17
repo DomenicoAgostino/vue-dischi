5c8b2e6346ccc8af8a05a6748d3390550daf05a7
DomenicoAgostino <99025293+DomenicoAgostino@users.noreply.github.com>
2022-05-17 17:00:50 +0200

    Inizio progetto

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="container">ciao</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+      return {
+        baseURL: 'https://flynn.boolean.careers/exercises/api/array/music',
+      }
+  },
+
+  component: {
+
+  },
+
+  mounted() {
+    this.getAPI()
+  },
+  methods:{
+    getAPI(){
+      axios.get(this.baseURL)
+      .then(r => {
+        console.log(r.data);
+      })
+    }
+      }
   }
-}
+
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/style/general'
 </style>
