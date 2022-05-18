73d8695b11f2fe951cda5845c66d3d2369b7be98
DomenicoAgostino <99025293+DomenicoAgostino@users.noreply.github.com>
2022-05-18 17:22:41 +0200

    Sistemato CardComponent

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,34 +1,43 @@
 <template>
-  <div>
-    cards
+ <div class="col my-3">
+  <div class="smv-card d-flex flex-column  align-items-center p-2 text-center">
+    <img :src="card.poster" :alt="card.poster" class="mt-2">
+    <h4 class="my-2">{{card.title}}</h4>
+    <p>{{card.author}}</p>
+    <p>{{card.year}}</p>
+
   </div>
+
+</div>
 </template>
 
 <script>
-import axios from 'axios';
 export default {
-  name: 'CardComp',
-  data() {
-    return {
-      apiUrl: 'https://flynn.boolean.careers/exercises/api/array/music'
-    }
-  },
+  name:'CardComp',
+  props:{
+    card: Object
+  }
 
-  mounted() {
-    this.getApi();
-  },
-
-  methods: {
-    getApi(){
-      axios.get(this.apiUrl)
-      .then(response =>{
-        console.log(response.data);
-      })
-    }
-  },
 }
+
 </script>
 
 <style lang="scss" scoped>
 
+@import '../assets/style/vars';
+
+.smv-card{
+  width: 100%;
+  height: 400px;
+  background-color: $primary-color;
+  img{
+    width: 90%;
+  }
+  h4{
+    color: white;
+  }
+  p{
+    color: gray;
+  }
+}
 </style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,16 +1,40 @@
 <template>
-  <main>
-    <div class="container">
-      <CardComponent/>
+<main>
+   <div class="container p-5">
+    <div class="row row-cols-5">
+      <CardComponent />
     </div>
-  </main>
+
+  </div>
+</main>
+ 
 </template>
 
 <script>
+
+
+import axios from 'axios';
 import CardComponent from './CardComponent.vue';
 export default {
-  name: 'MainComponent',
-  components: { CardComponent }
+    name: "MainComponent",
+    data() {
+        return {
+            apiUrl: "https://flynn.boolean.careers/exercises/api/array/music",
+            musiCards: [],
+        };
+    },
+    mounted() {
+        this.getApi();
+    },
+    methods: {
+        getApi() {
+            axios.get(this.apiUrl)
+                .then(response => {
+                this.musicCards = response.data;
+            });
+        }
+    },
+    components: { CardComponent }
 }
 </script>
 
